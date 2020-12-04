5121919bd9cb76111d24ba66e8fc0d234eaaa57e
nk512 <71583254+nk512@users.noreply.github.com>
2020-12-04 20:11:19 +0530

    added code

---
+++ javascriptDates.js
@@ -0,0 +1,7 @@
+function getDayName(dateString) {
+    let dayName;
+    var day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var date = new Date(dateString);
+    dayName = day_names[date.getDay()];
+    return dayName;
+}
